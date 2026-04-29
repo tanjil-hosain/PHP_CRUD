27188bd52159b0383b80c1f71e06739273967568
tanjil-hosain <tanjilh85@gmail.com>
2026-04-29 10:27:20 +0600

    updated

--- update.php
+++ update.php
@@ -3,10 +3,18 @@ $db=mysqli_connect("localhost", "root", "", "crud");
 
 $id = $_GET['id'];
 
-$data = mysqli_query($id,"SELECT * FROM info WHERE id = $id");
+$data = mysqli_query($db,"SELECT * FROM info WHERE id = $id");
 $row = mysqli_fetch_assoc($data);
 
-if(isset($_POST['update']))
+if(isset($_POST['update'])){
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+
+    $sql = "UPDATE info SET name = '$name', number = '$number' WHERE id ='$id'";
+     if(mysqli_query($db, $sql)){
+        header("location:view.php");
+     }
+}
 ?>
 
 <form action="" method="post">
@@ -14,5 +22,5 @@ if(isset($_POST['update']))
     <input type="text" name="name" value="<?php echo $row['name']; ?>"><br> <br>
     Number: <br>
     <input type="text" name="number" value="<?php echo $row['number']; ?>"> br <br>
-    <input type="submit" name="upadte" value="Update"> 
+    <input type="submit" name="update" value="Update"> 
 </form>
